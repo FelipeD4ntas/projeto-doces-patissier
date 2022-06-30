f74b3b2cbfab27640a2418df570bedabfa72a1f9
Marcos Felipe Dantas da Costa <felipeicedantas@gmail.com>
2022-06-30 17:27:17 -0300

    up

--- js/pagina-login.js
+++ js/pagina-login.js
@@ -1,14 +1,47 @@
-const btnSeInscrever = document.querySelector('[data-js="botao-se-inscrever"]');
-const boxForm = document.querySelector('[data-js="box-form-inscricao"]');
-const conteudoBtnSignup = document.querySelector('[data-js="cont-signup"]');
-const loadingSignup = document.querySelector('[data-js="loading-signup"]');
+const btnSeInscrever = document.querySelectorAll('[data-js="botao-se-inscrever"]');
+const btnLogin = document.querySelectorAll('[data-js="botao-login"]');
+const boxFormSeInscrever = document.querySelector('[data-js="box-form-inscricao"]');
+const boxFormLogin = document.querySelector('[data-js="box-form-login"]');
+const boxPrincipal = document.querySelector('[data-js="box-principal"]');
+const conteudoBtnSignup = document.querySelectorAll('[data-js="cont-signup"]');
+const conteudoBtnLogin = document.querySelectorAll('[data-js="cont-login"]');
+const loadingSignup = document.querySelectorAll('[data-js="loading-signup"]');
+const loadingLogin = document.querySelectorAll('[data-js="loading-login"]');
 
 function seIncrever() {
-  conteudoBtnSignup.style.display = 'none';
-  loadingSignup.style.display = 'block';
+  conteudoBtnSignup.forEach(cont => {
+    cont.style.display = 'none';
+  })
+  loadingSignup.forEach(loading => {
+    loading.style.display = 'block';
+  })
+
   setTimeout(() => {
-    boxForm.style.left = '0';
+    boxFormSeInscrever.style.left = '0';
+    boxFormLogin.style.right = '2000px';
+    boxPrincipal.style.opacity = '0';
   }, 1000);
 };
 
-btnSeInscrever.addEventListener('click', seIncrever);
+function logar() {
+  conteudoBtnLogin.forEach(cont => {
+      cont.style.display = 'none';
+  });
+  loadingLogin.forEach(loading => {
+    loading.style.display = 'block';
+  });
+
+  setTimeout(() => {
+    boxFormSeInscrever.style.left = '2000px';
+    boxFormLogin.style.right = '0';
+    boxPrincipal.style.opacity = '0';
+  }, 1000);
+}
+
+btnSeInscrever.forEach(btn => {
+  btn.addEventListener('click', seIncrever);
+});
+
+btnLogin.forEach(btn => {
+  btn.addEventListener('click', logar);
+})
